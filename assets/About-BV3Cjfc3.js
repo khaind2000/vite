8b9b9f810ec8e5a4b319832162f70c9647c3e442
itKhaiNDdotDev <khai12345678t@gmail.com>
2025-09-04 17:03:48 +0700

    Updates

---
+++ assets/About-BV3Cjfc3.js
@@ -0,0 +1,6 @@
+
+/*!
+ * 🚀 Project: Vite + React + TS
+ * 📦 Build time: 9/4/2025, 5:03:30 PM
+ */
+import{j as t}from"./index-DWvwjmdB.js";function r(){return t.jsx("div",{children:"About"})}export{r as default};
---
+++ assets/Home-WiIjTCWb.js
@@ -0,0 +1,6 @@
+
+/*!
+ * 🚀 Project: Vite + React + TS
+ * 📦 Build time: 9/4/2025, 5:03:30 PM
+ */
+import{j as e}from"./index-DWvwjmdB.js";function r(){return e.jsx("div",{children:"Home"})}export{r as default};
